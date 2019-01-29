6d9b15f12b033b6e61efe9078faaaf028940d18e
Adrian Gawor <adrian.gawor@htl.rennweg.at>
2019-01-29 09:51:22 +0100

    Added queryBuilder.

--- console.php
+++ console.php
@@ -5,3 +5,11 @@ require __DIR__ . '/config/settings.php';
 
 $config = new \Doctrine\DBAL\Configuration();
 $conn = \Doctrine\DBAL\DriverManager::getConnection($connectionParams, $config);
+
+$queryBuilder = $conn->createQueryBuilder();
+
+$queryBuilder
+    ->select('id', 'username', 'password')
+    ->from('demo');
+
+print_r($queryBuilder->execute()->fetchAll());
